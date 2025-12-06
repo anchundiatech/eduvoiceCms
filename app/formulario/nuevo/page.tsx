bc10c85611721c6a941bad346d892fc85d31f3e7
Alejandro <alejodev2022@gmail.com>
2025-12-05 19:41:04 -0500

    refactor: actualizar el tipo de searchParams en NewFormularioPage para que sea una promesa

--- app/formulario/nuevo/page.tsx
+++ app/formulario/nuevo/page.tsx
@@ -1,11 +1,11 @@
 import FormularioBuilderPage from "../[slug]/builder/page";
 
 type NewFormularioPageProps = {
-  searchParams?: {
+  searchParams?: Promise<{
     categoriaId?: string;
-  };
+  }>;
 };
 
 export default function NewFormularioPage({ searchParams }: NewFormularioPageProps) {
-  return <FormularioBuilderPage categoriaId={searchParams?.categoriaId} />;
+  return <FormularioBuilderPage params={Promise.resolve({ slug: 'nuevo' })} searchParams={searchParams} />;
 }
