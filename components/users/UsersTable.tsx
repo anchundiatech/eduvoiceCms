be9656bca64d1f5b381f4da5f737c945348c1064
Aracely Cruz <yuki_oh91@hotmail.com>
2025-12-08 14:00:55 -0600

    Vista detalle de usuario añadida

--- components/users/UsersTable.tsx
+++ components/users/UsersTable.tsx
@@ -1,11 +1,9 @@
 "use client";
-
-import Image from "next/image";
-import { UserType } from "@/types/user";
-import { Pencil, Trash2 } from "lucide-react";
-import { mockUsers } from "./mockUsers"
+import { UserType } from '@/types/user';
+import { mockUsers } from './mockUsers';
 import { InviteMemberModal } from './InviteMemberModal';
 import { useState } from 'react';
+import { UserRow } from './UserRow';
 
 export default function UsersTable({
   users = mockUsers,
@@ -53,41 +51,7 @@ export default function UsersTable({
 
         <tbody className="text-sm">
           {users.map((user) => (
-            <tr key={user.id} className="border-b hover:bg-gray-50 transition">
-              <td className="p-4 flex items-center gap-3">
-                <img
-                  src={user.avatar}
-                  alt={user.name}
-                  className="w-10 h-10 rounded-full"
-                />
-                <span className="font-medium text-[#111618]">{user.name}</span>
-              </td>
-
-              <td className="p-4 text-gray-700">{user.email}</td>
-              <td className="p-4 capitalize text-gray-700">{user.role}</td>
-
-              <td className="p-4">
-                <span
-                  className={`px-3 py-1 rounded-full text-xs font-semibold
-${user.status === 'active' ? 'bg-green-200 text-green-800' : ''}
-${user.status === 'pending' ? 'bg-yellow-200 text-yellow-800' : ''}
-${user.status === 'inactive' ? 'bg-red-200 text-red-800' : ''}`}
-                >
-                  {user.status}
-                </span>
-              </td>
-
-              <td className="p-4 text-gray-700">{user.createdAt}</td>
-
-              <td className="p-4 flex justify-center gap-4">
-                <button className="p-2 bg-blue-100 text-blue-700 rounded-lg hover:bg-blue-200">
-                  <Pencil size={16} />
-                </button>
-                <button className="p-2 bg-red-100 text-red-700 rounded-lg hover:bg-red-200">
-                  <Trash2 size={16} />
-                </button>
-              </td>
-            </tr>
+            <UserRow key={user.id} user={user} />
           ))}
         </tbody>
       </table>
